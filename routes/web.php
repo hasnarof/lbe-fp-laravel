882e4b7bc29be728a327b5a3931aa6bb659c689f
hasnarof <hasnarof@gmail.com>
2020-11-14 14:32:25 +0700

    show data

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,12 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+//route CRUD
+Route::get('/mobil', 'MobilController@index');
