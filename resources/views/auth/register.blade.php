c8717827bb402fbdc5e92c222b787d32784da092
Drigo Alexander <drigosihombinga@gmail.com>
2020-11-14 15:33:53 +0700

    ui

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+
+<style>
+
+body{
+    margin: 0;
+}
+
+</style>
+
+
+</head>
+<body>
+    
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -75,3 +96,7 @@
     </div>
 </div>
 @endsection
+
+</body>
+</html>
+
